9649686b456b8a6412f168f7e886455d87a9b57f
Veanty <plmulticraftpl@gmail.com>
2022-06-03 17:39:16 +0200

    Major improvements to readability, optimalization and added visibility to html

--- js.js
+++ js.js
@@ -34,49 +34,39 @@ function playRound(playerSelection, computerSelection) {
     console.log("It's a tie!");
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     console.log("You win! Rock beats scissors");
+    playerScore++;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     console.log("You win! Paper beats rock");
+    playerScore++;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     console.log("You win! Scissors beats paper");
+    playerScore++;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     console.log("You lose! Paper beats rock");
+    computerScore++;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     console.log("You lose! Scissors beats paper");
+    computerScore++;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     console.log("You lose! Rock beats scissors");
+    computerScore++;
   }
 }
+
 function playGame() {
   let howManyRounds = +prompt("How many rounds do you want to play?");
+  let roundsLeft;
   playerScore = 0;
   computerScore = 0;
-  for (let i = 0; i < howManyRounds; i++) {
+  for (roundsLeft = howManyRounds; roundsLeft >= 0; roundsLeft--) {
     let choice = prompt("What you gonna play?");
     const playerSelection = playerPlay(choice);
     const computerSelection = computerPlay();
     playRound(playerSelection, computerSelection);
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-      playerScore++;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-      playerScore++;
-    } else if (
-      playerSelection === "scissors" &&
-      computerSelection === "paper"
-    ) {
-      playerScore++;
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-      computerScore++;
-    } else if (
-      playerSelection === "paper" &&
-      computerSelection === "scissors"
-    ) {
-      computerScore++;
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-      computerScore++;
-    }
     console.log(
       `Player score: ${playerScore} || Computer score: ${computerScore}`
     );
+    roundsPara.textContent = `Rounds left: ${roundsLeft}`;
   }
   console.log(`Total Player score: ${playerScore}
   Total Computer score: ${computerScore}`);
@@ -89,4 +79,8 @@ function playGame() {
   }
 }
 
-console.log(playGame());
+const roundsPara = document.querySelector("p.rounds");
+const startButton = document.querySelector("button.startGame");
+const roundResults = document.querySelector("p.roundResult");
+
+startButton.addEventListener("click", playGame);
